16f370e3beb3b33ca43f81d69a6e1d71244b9dd6
Buco7854 <arnaud@grimbert.com>
2024-07-09 01:05:19 +0200

    Refactor, fix bug regarding settings reset, and add new /api/ports to get ports

--- utils/database/__init__.py
+++
@@ -1,7 +0,0 @@
-# utils/database/__init__.py
-
-from .db import db, init_db, create_tables
-from .port import Port
-from .setting import Setting
-
-__all__ = ['db', 'init_db', 'create_tables', 'Port', 'Setting']
--- utils/database/setting.py
+++
@@ -1,8 +0,0 @@
-# utils/database/setting.py
-
-from .db import db
-
-class Setting(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String(50), unique=True, nullable=False)
-    value = db.Column(db.String(100), nullable=False)
