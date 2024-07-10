2c58098749b46ce74b4aabe7253d00c6bd5f1809
Idleite <harryessesdev@jimmysmp.com>
2024-07-10 03:33:46 +0000

    chnaged db model to fix TCP duplicate

--- utils/database/port.py
+++ utils/database/port.py
@@ -5,8 +5,8 @@ from .db import db
 class Port(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ip_address = db.Column(db.String(15), nullable=False)
-    nickname = db.Column(db.String(50))
-    port_number = db.Column(db.Integer, nullable=False)
+    nickname = db.Column(db.String(50), nullable=True)
+    port_number = db.Column(db.String, nullable=False)
     description = db.Column(db.String(100), nullable=False)
     order = db.Column(db.Integer, default=0)
 
