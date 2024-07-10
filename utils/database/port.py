9256bd4e5aee8efdef52b59c285c541f5ed55cbf
Idleite <harryessesdev@jimmysmp.com>
2024-07-10 03:02:26 +0000

    temp commit

--- utils/database/port.py
+++ utils/database/port.py
@@ -5,9 +5,10 @@ from .db import db
 class Port(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ip_address = db.Column(db.String(15), nullable=False)
-    nickname = db.Column(db.String(50))
+    nickname = db.Column(db.String(50), nullable=True)
     port_number = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(100), nullable=False)
     order = db.Column(db.Integer, default=0)
+    docker_id = db.Column(db.String(20), nullable=True)
 
     __table_args__ = (db.UniqueConstraint('ip_address', 'port_number', name='_ip_port_uc'),)
---
+++ utils/docker/socket.py
@@ -0,0 +1,43 @@
+# External Imports
+from flask import current_app as app            # For accessing the Flask app
+import docker
+from utils.database import db, Port, Setting    # For accessing the database models
+def access_docker_socket():
+    client = docker.from_env()
+    for container in client.containers.list():
+        for key in container.ports:
+            if container.ports[key] == None:
+                continue
+            else:
+              try:
+                  ip = str(container.labels["com.portall.ip"])
+              except:
+                  ip = "127.0.0.1"
+              try:
+                  description = str(container.labels["com.portall.description"])
+              except:
+                  description = str(container.name)
+              write_port(ip, int(container.ports[key][0]['HostPort']), description, container.id)
+
+def write_port(ip, port, description, docker_id):
+    try:
+        port_entry = Port.query.filter_by(docker_id=docker_id).one()
+        if not port_entry:
+            port = Port(ip_address=ip, port_number=port, description=description)
+            db.session.add(port)
+
+        else:
+            # Check if the new port number already exists for this IP
+            existing_port = Port.query.filter(Port.ip_address == ip,
+                                              Port.port_number == port, 
+                                              Port.docker_id != docker_id ).first()
+            if existing_port:
+                app.logger.info(f"Warning: Port {port} already exists on this ip")
+
+        port_entry.port_number = port
+        port_entry.description = description
+        port_entry.ip = ip
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        app.logger.info(f"Error: There Was an error whilst Accessing The Docker Socket")
