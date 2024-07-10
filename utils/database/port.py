d9fa38f794d756f044856ed3db0f92d4e0bc23c4
Idleite <harryessesdev@jimmysmp.com>
2024-07-10 20:12:29 +0000

    added tcp fix

--- utils/database/port.py
+++ utils/database/port.py
@@ -6,7 +6,7 @@ class Port(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ip_address = db.Column(db.String(15), nullable=False)
     nickname = db.Column(db.String(50), nullable=True)
-    port_number = db.Column(db.Integer, nullable=False)
+    port_number = db.Column(db.String, nullable=False)
     description = db.Column(db.String(100), nullable=False)
     order = db.Column(db.Integer, default=0)
     docker_id = db.Column(db.String(20), nullable=True)
