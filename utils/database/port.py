ec6299bc5cdbec890978d582b23c7331d49c8bb3
Need4Swede <theneed4swede@gmail.com>
2024-07-10 17:33:25 -0700

    fixed nickname nullable

--- utils/database/port.py
+++ utils/database/port.py
@@ -5,7 +5,7 @@ from .db import db
 class Port(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ip_address = db.Column(db.String(15), nullable=False)
-    nickname = db.Column(db.String(50), nullable=False)
+    nickname = db.Column(db.String(50))
     port_number = db.Column(db.Integer, nullable=False)
     port_protocol = db.Column(db.String(3), nullable=False)
     description = db.Column(db.String(100), nullable=False)
