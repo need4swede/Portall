f727deac8d3ebd793d758f35ed11fe4c88a2e78f
Need4Swede <theneed4swede@gmail.com>
2024-07-12 18:12:46 -0700

    I'm an idiot (removed unique property)

--- utils/database/port.py
+++ utils/database/port.py
@@ -4,7 +4,7 @@ from .db import db
 
 class Port(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ip_address = db.Column(db.String(15), nullable=False, unique=True)
+    ip_address = db.Column(db.String(15), nullable=False)
     nickname = db.Column(db.String(50))
     port_number = db.Column(db.Integer, nullable=False)
     port_protocol = db.Column(db.String(3), nullable=False)
